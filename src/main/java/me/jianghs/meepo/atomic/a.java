600a6345c67ce15ba3878a49f92af3a334d283ff
jianghs430 <jianghs430@zto.com>
2021-01-28 18:02:02 +0800

    test

---
+++ src/main/java/me/jianghs/meepo/atomic/a.java
@@ -0,0 +1,11 @@
+package me.jianghs.meepo.atomic;
+
+/**
+ * @className: a
+ * @description:
+ * @author: jianghs430
+ * @createDate: 2021/1/28 17:59
+ * @version: 1.0
+ */
+public class a {
+}
---
+++ src/main/java/me/jianghs/meepo/synchronizedtest/a.java
@@ -0,0 +1,11 @@
+package me.jianghs.meepo.synchronizedtest;
+
+/**
+ * @className: a
+ * @description:
+ * @author: jianghs430
+ * @createDate: 2021/1/28 18:01
+ * @version: 1.0
+ */
+public class a {
+}
